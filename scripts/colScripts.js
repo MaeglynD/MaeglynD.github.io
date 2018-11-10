6c716a42063b1b07796b0eac335f9e5a118bed1a
MaeglynD <maeglyn.davies@gmail.com>
2018-11-10 18:47:19 +0000

    REHAUL update

--- scripts/colScripts.js
+++ scripts/colScripts.js
@@ -1,12 +1,4 @@
-var rq1 = document.getElementById("rq1");
-var rq2 = document.getElementById("rq2");
-var rq3 = document.getElementById("rq3");
-var rq4 = document.getElementById("rq4");
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-var modal = document.getElementById('myModal');
-var close = document.getElementById("close");
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -35,26 +27,20 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-rq1.onclick = function(){
-modal.style.display = "block";
-  modalImg.src = "../assets/oldcolLogin.png";
-  captionText.innerHTML = "Old COL8 Login page";
+
+
+prev.onclick = function(){
+  plusSlides(-1);
 }
-rq2.onclick = function(){
-modal.style.display = "block";
-  modalImg.src = "../assets/newcolLogin.png";
-  captionText.innerHTML = "New COL8 Login page";
+next.onclick = function(){
+  plusSlides(1);
 }
-rq3.onclick = function(){
-modal.style.display = "block";
-  modalImg.src = "../assets/oldcolUpload.png";
-  captionText.innerHTML = "Old COL8 Upload page";
+dot1.onclick = function(){
+  currentSlide(1);
 }
-rq4.onclick = function(){
-modal.style.display = "block";
-  modalImg.src = "../assets/newcolUpload.png";
-  captionText.innerHTML = "New COL8 Upload page";
+dot2.onclick = function(){
+  currentSlide(2);
 }
-close.onclick = function() {
-modal.style.display = "none";
+dot3.onclick = function(){
+  currentSlide(3);
 }
