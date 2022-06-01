66c7ee71423ce27ba0206b19ccc8f4760756a974
MaeglynD <maeglyn.davies@gmail.com>
2022-06-01 22:42:35 +0100

    cleaned up home page

--- old/_nuxt/static/1617290404/projects/infinite-typeracer/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/projects/Infinite-typeracer", {data:[{},{}],fetch:{},mutations:void 0});
--- old/_nuxt/static/1617290404/projects/telematics-software/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/projects/Telematics-software", {data:[{},{}],fetch:{},mutations:void 0});
