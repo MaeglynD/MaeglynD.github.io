489070ae2ebf750dd0d77d739f47083d53d4e851
MaeglynD <maeglyn.davies@gmail.com>
2025-03-07 14:36:49 +0000

    order change on blog

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -10,19 +10,13 @@ export default function Blog() {
       <div className={s.blog}>
         <div className={s.header}>Posts</div>
 
-        <Link href="/blog/fluid-dynamics" className={s.blogLink}>
+        <Link href="/blog/visualising-hydrogen" className={s.blogLink}>
           <div className={s.blogText}>
-            <div className={s.blogHead}>Checking in with the fluid dynamicists (and some light grmhd)</div>
+            <div className={s.blogHead}>Visualising hydrogenic energy eigenfunctions and spherical harmonics</div>
 
-            <div className={s.blogDate}>17 Feb, 2025</div>
+            <div className={s.blogDate}>27 Dec, 2024</div>
           </div>
-          <Image
-            className={s.thumb}
-            src="/fluid-dynamics/thumb.png"
-            width={400}
-            height={400}
-            alt="fluid dynamics blog"
-          />
+          <Image className={s.thumb} src="/hydrogen/thumb.png" width={400} height={400} alt="hydrogen blog" />
         </Link>
 
         <Link href="/blog/fourier" className={s.blogLink}>
@@ -36,13 +30,19 @@ export default function Blog() {
           <Image className={s.thumb} src="/fourier/thumb.png" width={400} height={400} alt="fourier blog" />
         </Link>
 
-        <Link href="/blog/visualising-hydrogen" className={s.blogLink}>
+        <Link href="/blog/fluid-dynamics" className={s.blogLink}>
           <div className={s.blogText}>
-            <div className={s.blogHead}>Visualising hydrogenic energy eigenfunctions and spherical harmonics</div>
+            <div className={s.blogHead}>Checking in with the fluid dynamicists (and some light grmhd)</div>
 
-            <div className={s.blogDate}>27 Dec, 2024</div>
+            <div className={s.blogDate}>17 Feb, 2025</div>
           </div>
-          <Image className={s.thumb} src="/hydrogen/thumb.png" width={400} height={400} alt="hydrogen blog" />
+          <Image
+            className={s.thumb}
+            src="/fluid-dynamics/thumb.png"
+            width={400}
+            height={400}
+            alt="fluid dynamics blog"
+          />
         </Link>
       </div>
     </div>
